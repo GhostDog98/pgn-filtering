d01b31b6e311f9ecf7d6f734a27d6070dc791b9f
Lilly <lilly@MacBook-Pro.local>
2024-09-29 14:18:02 +1000

    more speed

--- main.c
+++ main.c
@@ -4,48 +4,44 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
-
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 static char printf_buffer[160000000]; // size of printf buffer
-#define ACCUMULATOR_ITEMS 32
-#define ACCUMULATOR_ITEM_MAX_LENGTH 32768
+#define ACCUMULATOR_ITEMS 40  // Maximum number of tags in a game, plus 3 for the newlines and pgn data
+#define ACCUMULATOR_ITEM_MAX_LENGTH 19202 // Max lichess game length plus worst case pgn notation https://lichess.org/forum/general-chess-discussion/what-is-the-highest-number-of-moves-played-in-a-game-on-lichess
 
-// Function to extract Elo rating from a line
+// Function to extract Elo rating from a line, tell gcc it's static (not called from anything else) and should be inlined for speeeeed
 int extract_elo(char *line) {
-    char *elo_str = strstr(line, "\"");
-    if (elo_str) {
-        return atoi(elo_str + 1);
-    }
-    return 0;
+    char *elo_str = strstr(line, "\""); // strstr returns us a pointer of the first character that has a \" character
+    return atoi(elo_str + 1);
 }
 
 
 // Function to check if both WhiteElo and BlackElo are within the target Elo range
 bool is_within_elo_range(char accumulator[ACCUMULATOR_ITEMS][ACCUMULATOR_ITEM_MAX_LENGTH], int low_elo, int high_elo) {
-    int white_elo = 0;
-    int black_elo = 0;
 
-    int early_exit = 0;
+    for (int i = 0; i < ACCUMULATOR_ITEMS; i++){
+        if (strstr(accumulator[i], "WhiteElo")){
+            printf("Found white on line %i\n", i);
+            int white_elo = extract_elo(accumulator[i]);
+            // We know that the black elo HAS TO BE NEXT, saves a bunch of time
+            int black_elo = extract_elo(accumulator[i + 1]);
 
-    for (int i = 0; i < ACCUMULATOR_ITEMS; i++) {  // Iterate over the entire accumulator to see if we have WhiteElo or BlackElo.
-        if (strstr(accumulator[i], "WhiteElo")) {
-            white_elo = extract_elo(accumulator[i]);
-            early_exit++;
-        } else if (strstr(accumulator[i], "BlackElo")) {
-            black_elo = extract_elo(accumulator[i]);
-            early_exit++;
-        }
-
-        if (early_exit >= 2) {  // If we have parsed 2 elo's, we shouldn't ever have more!
             return (white_elo >= low_elo && white_elo <= high_elo && black_elo >= low_elo && black_elo <= high_elo);
         }
     }
     printf("WARNING: RECORD REACHED END WITHOUT ELO");
+    exit(EXIT_FAILURE);
     return false;
 }
 
+
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
+    if (__builtin_expect(argc != 4, 0)) { // We expect this to usually pass through...
         printf("Usage: %s <low_elo> <high_elo> <file_to_filter.pgn>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -54,59 +50,86 @@ int main(int argc, char *argv[]) {
     int high_elo = atoi(argv[2]);
     char *file = argv[3];
 
-    char accumulator[ACCUMULATOR_ITEMS][ACCUMULATOR_ITEM_MAX_LENGTH];  // Array of 32 items (lines), each having 32k items (chars)
+    // Open file
+    int filedescriptor = open(file, O_RDONLY);
+    if (filedescriptor < 0) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get file size
+    struct stat stats;
+    fstat(filedescriptor, &stats);
+    size_t filesize = stats.st_size;
+
+    // Memory-map the file
+    char *file_data = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, filedescriptor, 0);
+    if (file_data == MAP_FAILED) {
+        perror("Error with mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    // Advise the kernel about the memory access pattern
+    if (madvise(file_data, filesize, MADV_SEQUENTIAL | MADV_WILLNEED) != 0) {
+        perror("Error with madvise");
+        exit(EXIT_FAILURE);
+    }
+
+    char accumulator[ACCUMULATOR_ITEMS][ACCUMULATOR_ITEM_MAX_LENGTH];  // Array of 32 items (lines), each having 19202 items (chars)
     int accumulator_index = 0;   // Index to keep track of the accumulator
     unsigned long long records_parsed = 0;
-    FILE *fp;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t linelength;
 
     // Set buffer to make printf faster
     setvbuf(stdout, printf_buffer, _IOFBF, sizeof(printf_buffer));
 
-    fp = fopen(file, "r");
-    if (fp == NULL) {
-        printf("Error opening file %s...\n", file);
-        exit(EXIT_FAILURE);
-    }
-
     bool first_newline = true;
-    while ((linelength = getline(&line, &len, fp)) != -1) {  // Read through entire file
-        if (linelength == 1 && first_newline == false) {  // We are at the end of a record now
-            records_parsed++;
+    char *line_start = file_data;
+    char *line_end = file_data;
 
-            // Check if the record is within the target Elo range
-            if (is_within_elo_range(accumulator, low_elo, high_elo)) {
-                printf("\n");  // Only print newline if a match is found, this ensures we have that extra break between pgn data and pgn moves
+    while (line_end < file_data + filesize) {
+        if (*line_end == '\n') {  // Found end of line
+            size_t linelength = line_end - line_start + 1;
 
-                // Print accumulator
-                for (int i = 0; i < accumulator_index; i++) {
-                    printf("%s", accumulator[i]);
+            if (linelength == 1 && first_newline == false) {  // End of a record
+                records_parsed++;
+
+                // Check if the record is within the target Elo range
+                if (is_within_elo_range(accumulator, low_elo, high_elo)) {
+                    printf("\n");  // Only print newline if a match is found
+
+                    // Print accumulator
+                    for (int i = 0; i < accumulator_index; i++) {
+                        printf("%s", accumulator[i]);
+                    }
+                }
+
+                // Clear the accumulator
+                accumulator_index = 0;
+                first_newline = true;
+            } else if (linelength == 1 && first_newline == true) {  // First newline separating tags and moves
+                first_newline = false;
+            } else {  // Inside a record
+                if (accumulator_index < ACCUMULATOR_ITEMS) {  // Add line to accumulator
+                    memcpy(accumulator[accumulator_index], line_start, linelength);
+                    accumulator[accumulator_index][linelength] = '\0';
+                    accumulator_index++;
+                } else {
+                    printf("Accumulator overflow!\n");
+                    break;
                 }
             }
-            // Clear the accumulator. We can leave the array full of prior data cos we keep track of ends of things, and use null terminated strings!
-            accumulator_index = 0;
 
-            first_newline = true;
-        } else if (linelength == 1 && first_newline == true) {  // We just passed the first newline seperating the moves and pgn tags
-            first_newline = false;
-        } else {  // we are in the middle of the record
-            // Append new `line` to accumulator
-            if (accumulator_index < ACCUMULATOR_ITEMS) { // Sanity check for if we are exceeding the tag limit. This "should" only happen if we somehow miss a newline, and overflow to the next record
-                memcpy(accumulator[accumulator_index], line, linelength);
-                accumulator[accumulator_index][linelength] = '\0';
-                accumulator_index++;
-            } else {
-                printf("Accumulator overflow!\n");
-                break;
-            }
+            line_start = line_end + 1;  // Move to next line
         }
+
+        line_end++;
     }
+
     printf("Total records parsed: %llu\n", records_parsed);
 
-    fclose(fp);
-    if (line)  // If we have hit the end
-        free(line);
+    // Cleanup
+    munmap(file_data, filesize);
+    close(filedescriptor);
+
     exit(EXIT_SUCCESS);
 }
